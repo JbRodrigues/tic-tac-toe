d6520866b368c969f947aabef906bfd45afdf95a
Julio Bisaia Rodrigues <juliobisaia@hotmail.com>
2022-06-11 14:38:14 -0300

    Update

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const btTE = document.querySelector('#te');
-const btTM = document.querySelector('#tm');
-const btTD = document.querySelector('#td');
+let topLeft = document.querySelector('#te');
+topLeft.innerHTML = "";
+let topMid = document.querySelector('#tm');
+topMid.innerHTML = "";
+let topRight = document.querySelector('#td');
+topRight.innerHTML = "";
 
-const btME = document.querySelector('#me');
-const btMM = document.querySelector('#mm');
-const btMD = document.querySelector('#md');
+let midLeft = document.querySelector('#me');
+midLeft.innerHTML = "";
+let midMid = document.querySelector('#mm');
+midMid.innerHTML = "";
+let midRight = document.querySelector('#md');
+midRight.innerHTML = "";
 
-const btBE = document.querySelector('#be');
-const btBM = document.querySelector('#bm');
-const btBD = document.querySelector('#bd');
+let botLeft = document.querySelector('#be');
+botLeft.innerHTML = "";
+let botMid = document.querySelector('#bm');
+botMid.innerHTML = "";
+let botRight = document.querySelector('#bm');
+botRight.innerHTML = "";
 
 const btStart = document.querySelector('#start');
 const btReset = document.querySelector('#reset');
@@ -16,13 +25,19 @@ const btReset = document.querySelector('#reset');
 let xis = document.querySelector('#x');
 let zero = document.querySelector('#z');
 
+let game = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+];
 
+let player = 'O';
 
 btReset.style.display = 'none';
 
 let isPlaying = false;
 
-function iniciar() {
+function start() {
     isPlaying = true;
     xis.innerHTML = 0;
     zero.innerHTML = 0;
@@ -31,10 +46,23 @@ function iniciar() {
     btStart.style.display = 'none';
 }
 
-function resetar() {
+function reset() {
     isPlaying = false
     btStart.style.display = 'block';
     btReset.style.display = 'none';
+    
+    topLeft.innerHTML = "";
+    topMid.innerHTML = "";
+    topRight.innerHTML = "";
+
+    midLeft.innerHTML = "";
+    midMid.innerHTML = "";
+    midRight.innerHTML = "";
+
+    botLeft.innerHTML = "";
+    botMid.innerHTML = "";
+    botRight.innerHTML = "";
+    
 }
 
 function verificaClick() {
@@ -42,43 +70,7 @@ function verificaClick() {
     if (isPlaying == false) {
         alert("Inicie um novo Jogo!")
     } else {
-        btTE.addEventListener('click', () => {
-            console.log("Clicou no TE")
-        })
-        btTM.addEventListener('click', () => {
-            console.log("Clicou no TM")
-        })
-        btTD.addEventListener('click', () => {
-            console.log("Clicou no TD")
-        })
-
-        /*--------------------------------------------------------------*/
-        btME.addEventListener('click', () => {
-            console.log("Clicou no ME")
-        })
-        btMM.addEventListener('click', () => {
-            console.log("Clicou no MM")
-        })
-        btMD.addEventListener('click', () => {
-            console.log("Clicou no MD")
-        })
-
-        /*--------------------------------------------------------------*/
-        btBE.addEventListener('click', () => {
-            console.log("Clicou no BE")
-        })
-        btBM.addEventListener('click', () => {
-            console.log("Clicou no BM")
-        })
-        btBD.addEventListener('click', () => {
-            console.log("Clicou no BD")
-        })
+       let btn = document.querySelector('#te');
+       btn.innerHTML = "O"
     }
-
 }
-
-
-
-
-
-
